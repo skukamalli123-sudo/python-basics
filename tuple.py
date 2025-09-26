f239d3d8883a0aab0819dd33a532fbe32cad72a2
Shivakumar <skukamalli123@gmail.com>
2025-09-26 18:23:00 +0530

    learnt about the tuples

---
+++ tuple.py
@@ -0,0 +1,17 @@
+#TUPLE 
+#mainly used with comma 
+t1 = (1,2,3)
+
+print(t1)
+t2 = 1,2,3,4
+print(t2)
+print(t2[0])
+fruits = ("hello",)# without the last comma it is consider as string with comma it's a tuple
+print(fruits)
+
+normal_tuple = ("apple","banana")
+#to convert the tuple into list for the append insert pop many more method.
+newlist = list(normal_tuple)
+newlist.append("mango")
+print(newlist)
+
