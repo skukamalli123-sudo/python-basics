88993e2d4f1854c223e83b10058186c147ccbf55
Shivakumar <skukamalli123@gmail.com>
2025-09-28 13:00:11 +0530

    i used for to perfome the forloop

---
+++ forloop.py
@@ -0,0 +1,16 @@
+fruits = ["apple","mango","banana"]
+
+# if we use "for" in syntax it means it is a forloop
+for fruit in fruits:
+    print(fruit) # this syntax is used to print the loop of fruits as fruit if there is 3 item in list 
+#then the loop run for 3 times to check it print it
+
+    print(fruit,"is a fruit") # this syntax is used to add something to result here i added "is a fruit" 
+
+    print(fruit, "length is",len(fruit))
+    # the above syntax is used to print the length of particular item in list,
+
+    if len(fruit) >= 6:
+        print(fruit)
+    # the above syntax is used to print the name of fruits having length >= to 6:
+
