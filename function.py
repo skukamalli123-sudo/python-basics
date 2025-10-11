ea5322ea833dbc24004eb1238ef2fd425eb9fd1b
Shivakumar <skukamalli123@gmail.com>
2025-10-11 12:24:58 +0530

    learnd abount function

---
+++ function.py
@@ -0,0 +1,48 @@
+def logmessage():
+    fruits = ["apple","mango"]
+    print(fruits)
+
+logmessage()
+
+def add():# it is hardcode function where we predifen the vale for variable
+    a = 10
+    b = 20
+    print (a+b)
+add()
+
+def sub():
+    print(10-20)
+sub()
+#or we can mention parameters in the empty parenthese after the we call the function n no. of times 
+#with different values
+def sub(a,b):
+    print(a-b)
+sub(10,20)
+
+def mul():# syntax for the no parameters
+    print (10*3)
+mul()
+
+    #OR
+
+def mul(a,b):#syntax with parameters
+    print(a*b)
+mul(10,20)
+
+def div():# syntax for the no parameters
+    print (25/5)
+div()
+
+    #OR
+
+def div(a,b):#syntax with parameters
+    print(a/b)
+
+div(200,10)
+div(20,10)
+div(30,10)
+
+
+
+
+
