79f3e7b0076525db5dd10bff5d8b1ab413308f60
Shivakumar <skukamalli123@gmail.com>
2025-10-11 12:28:44 +0530

    learnd about the type casting

---
+++ type-casting.py
@@ -0,0 +1,24 @@
+# type casting or type converstion
+
+#int()
+print (int('10')) # str to int
+print (int(10.20)) # float to int
+
+#str()
+print(str(10))#int to str
+print (str(10.20)) # float to str
+
+#float
+print(float(10))#int to str
+print (float(10.20)) # float to str
+
+#list[]
+print(list(("a","b")))#tuple to list
+print(list({"a","b"}))#set to list
+
+#tuple()
+print(tuple['a','b'])# tuple to list
+print(tuple{'a','b'})# tuple to set.
+
+#set{}
+print(set[])
