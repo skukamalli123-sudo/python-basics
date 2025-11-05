447d19cd3777ff5914be440efb05714e81954c13
Shivakumar <skukamalli123@gmail.com>
2025-11-05 18:24:46 +0530

    dnskjgnkn

--- Pandas/functionss.py
+++ Pandas/functionss.py
@@ -5,3 +5,11 @@ def greet(name, is_morning):
         print ("Good evening",name)
 
 print(greet("shiva",False))
+
+def add(a,b,showresult):
+    if showresult == "add" :
+        print(a+b)
+    else:
+        print("no add")    
+
+add(10,10, "add")
